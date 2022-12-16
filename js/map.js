fd2619a6cc27d9c8d31edbfdac0b682d99304282
Bruna Oliveira <oliveira_bruna@outlook.com>
2022-12-16 15:47:09 -0300

    atualiza class logo e remove arquivos não utilizados

--- js/map.js
+++
@@ -1,9 +0,0 @@
-L.mapquest.key = 'o0zLYg8NnublMtdZggu5rzAmrX1u3U6j';
-
-        const map = L.mapquest.map('map', {
-          center: [37.7749, -122.4194],
-          layers: L.mapquest.tileLayer('map'),
-          zoom: 12
-        });
-
-        map.addControl(L.mapquest.control());
